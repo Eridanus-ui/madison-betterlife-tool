8805160ad59cd10db25eb851e6acd4610f71cdb5
PAUL KAMAU <njigupaul22@gmail.com>
2023-06-15 09:08:46 +0300

    updated background effect

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
 
 function App() {
   return (
-    <div className="App md:my-6 mx-auto sm:mt-1 sm:px-6 lg:px-8 bg-white dark:bg-slate-800 rounded-lg px-6 py-6 ring-1 ring-slate-900/5 shadow-2xl">
+    <div className="App md:my-6 mx-auto sm:mt-1 sm:px-6 lg:px-8 bg-slate-800 rounded-lg px-6 py-6 ring-1 ring-slate-900/5 shadow-2xl">
       <div className="flex justify-center  bg-white rounded-lg">
         <img
           src="Madison-Insurance-Company-Kenya3.png"
